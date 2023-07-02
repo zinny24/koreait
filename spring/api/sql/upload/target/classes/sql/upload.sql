9839b3e06cd150be31736967f8fc166d62f0ce8c
zinny24 <61670559+zinny24@users.noreply.github.com>
2023-07-03 08:42:37 +0900

    Delete spring/api/sql/upload directory

--- spring/api/sql/upload/target/classes/sql/upload.sql
+++
@@ -1,9 +0,0 @@
-use koreaitdb;
-
-
-create table uploads(
-    imageName varchar(255), -- 원본 이름
-    imageSize bigint, -- 용량
-    imageUrl varchar(500), -- 경로
-    imageTransName varchar(255) -- 변환한 이름
-);
