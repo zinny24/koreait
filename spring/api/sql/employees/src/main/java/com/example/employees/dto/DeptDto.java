0eb7e989ba8548fb77a87ac7303ee03a42c1a4f9
zinny24 <61670559+zinny24@users.noreply.github.com>
2023-07-03 16:45:20 +0900

    Add files via upload

---
+++ spring/api/sql/employees/src/main/java/com/example/employees/dto/DeptDto.java
@@ -0,0 +1,10 @@
+package com.example.employees.dto;
+
+import lombok.Data;
+
+@Data
+public class DeptDto {
+    private String korDeptCode;
+    private String korDeptName;
+    private int korPosCnt;
+}
