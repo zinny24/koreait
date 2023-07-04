eaa96c2ac37349cea513b0d59e347627274dc4b2
zinny24 <61670559+zinny24@users.noreply.github.com>
2023-07-04 09:54:34 +0900

    Add files via upload

---
+++ spring/api/sql/employees/src/main/java/com/example/employees/dto/PosDto.java
@@ -0,0 +1,11 @@
+package com.example.employees.dto;
+
+
+import lombok.Data;
+
+@Data
+public class PosDto {
+    private String korPosCode;
+    private String korPosName;
+    private String korDeptCode;
+}
