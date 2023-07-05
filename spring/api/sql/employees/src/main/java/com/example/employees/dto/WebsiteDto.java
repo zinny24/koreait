3306fd5bfa2c5dad22862aa826fb7aa7d362a397
zinny24 <61670559+zinny24@users.noreply.github.com>
2023-07-05 10:51:37 +0900

    Add files via upload

---
+++ spring/api/sql/employees/src/main/java/com/example/employees/dto/WebsiteDto.java
@@ -0,0 +1,14 @@
+package com.example.employees.dto;
+
+import lombok.Data;
+
+@Data
+public class WebsiteDto {
+    private String korWebLogo;
+    private String korWebTitle;
+    private String korWebMenus;
+    private String korWebUrl;
+    private String korWebCopyright;
+    private String korWebTerm;
+
+}
